d23c316ad8ed5e002a3f3289d1bd00e04c0f74c6
Bluegnarl <bluegnarl@gmail.com>
2023-11-16 13:53:41 +0100

    header responsive

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const logoHeader = document.querySelector('.logo-header');
 
 function onResize(){
-    if(document.body.clientWidth > 900){
+    if(document.body.clientWidth >= 800){
         logoHeader.setAttribute("src", "./assets/images/logo-large.svg");
     }
-    else if(document.body.clientWidth <= 900){
+    else if(document.body.clientWidth < 800){
         logoHeader.setAttribute("src", "./assets/images/logo-medium.svg");
     }
 }
