491ad15bdb46e5fab937b6a9a0055cdcc2a1aef7
Bluegnarl <dradarkx@gmail.com>
2023-11-17 00:33:21 +0100

    Favicon, header, first part responsive

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const logoHeader = document.querySelector('.logo-header');
+const arrowIcon = document.querySelector('.arrow');
 
 function onResize(){
     if(document.body.clientWidth >= 800){
         logoHeader.setAttribute("src", "./assets/images/logo-large.svg");
+        arrowIcon.setAttribute("src", "./assets/images/arrow-large.svg");
     }
     else if(document.body.clientWidth < 800){
         logoHeader.setAttribute("src", "./assets/images/logo-medium.svg");
+        arrowIcon.setAttribute("src", "./assets/images/arrow-small.svg");
     }
+
 }
 onResize();
 
